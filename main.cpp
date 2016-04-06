817dc3cd0a6437aa0f17b32738c5542c09b633bd
Maureenlc89 <mleonc@ucenfotec.ac.cr>
2016-04-05 20:59:18 -0600

    Menu

--- main.cpp
+++ main.cpp
@@ -11,13 +11,14 @@
  * Created on 2 de abril de 2016, 05:18 PM
  */
 
+#include <string>
 #include <cstdlib>
 #include <iostream>
 using namespace std;
 
-/*
- * 
- */
+int obtenerOpcion(void);
+bool ejecutar(int);
+
 int main(int argc, char** argv) {
 
 
@@ -30,22 +31,78 @@ int main(int argc, char** argv) {
      */
 
     //PRUEBA MOSTRAR MATRIZ
-    int vip [5][2] = {
-        {0, 0},
-        {1, 1},
-        {2, 2},
-        {3, 3},
-        {4, 4}};
+    //    int vip [5][2] = {
+    //        {0, 0},
+    //        {1, 1},
+    //        {2, 2},
+    //        {3, 3},
+    //        {4, 4}};
+    //
+    //    for (int i = 0; i < 5; i++) {
+    //        for (int j = 0; j < 2; j++) {
+    //            cout << "|" << vip[i][j];
+    //        }
+    //            cout << "|" << endl;
+    //    }
 
-    for (int i = 0; i < 5; i++) {
-        for (int j = 0; j < 2; j++) {
-            cout << "|" << vip[i][j];
+
+    int opcion;
+    string resul;
+    bool salir = false;
+    string menu[4] = {
+
+        "1.Reservar VIP ",
+        "2.Reservar Preferencial ",
+        "3.Reservar General",
+        "4.Salir"
+    };
+
+
+
+    do {
+        for (int i = 0; i < 5; i++) {
+
+            cout << menu[i] << endl;
         }
-            cout << "|" << endl;
-    }
-
+        opcion = obtenerOpcion();
+        salir = ejecutar(opcion);
+    } while (salir == false);
 
     return 0;
+}
+
+int obtenerOpcion() {
+    int opc;
+    cout << "Seleccione la opcion";
+    cin >> opc;
+
+    return opc;
+}
+
+bool ejecutar(int opcion) {
+    bool salir;
+    switch (opcion) {
+
+        case 1:
+            cout << "VIP" << endl;
+            break;
+        case 2:
+            cout << "Preferencial" << endl;
+            break;
+        case 3:
+            cout << "general" << endl;
+            break;
+        case 4:
+            cout << "adios" << endl;
+            salir = true;
+            break;
+
+        default:
+            break;
+
+
+    }
+    return salir;
 }
 
 
